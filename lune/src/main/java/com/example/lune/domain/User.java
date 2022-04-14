7ef49ef468c993bdda4f10f58eeb58a1001ffcb5
danta12 <name_Chloe@outlook.com>
2022-04-14 21:00:09 +0800

    domain

--- lune/src/main/java/com/example/lune/domain/User.java
+++ lune/src/main/java/com/example/lune/domain/User.java
@@ -1,4 +1,82 @@
 package com.example.lune.domain;
 
+import java.util.Date;
+
 public class User {
+    private Integer userId;
+    private String userName;
+    private String userPassword;
+    private String userAvatar;
+    private String userEmail;
+    private String userAuthority;
+    private Date userRegistrationTime;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserAvatar() {
+        return userAvatar;
+    }
+
+    public void setUserAvatar(String userAvatar) {
+        this.userAvatar = userAvatar;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserAuthority() {
+        return userAuthority;
+    }
+
+    public void setUserAuthority(String userAuthority) {
+        this.userAuthority = userAuthority;
+    }
+
+    public Date getUserRegistrationTime() {
+        return userRegistrationTime;
+    }
+
+    public void setUserRegistrationTime(Date userRegistrationTime) {
+        this.userRegistrationTime = userRegistrationTime;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", userAvatar='" + userAvatar + '\'' +
+                ", userEmail='" + userEmail + '\'' +
+                ", userAuthority='" + userAuthority + '\'' +
+                ", userRegistrationTime=" + userRegistrationTime +
+                '}';
+    }
 }
