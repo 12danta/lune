b6e7b222569f3c5acdebb235bc663a07512010ec
danta12 <name_Chloe@outlook.com>
2022-05-06 23:20:46 +0800

    md5

--- lune/src/main/java/com/example/lune/config/encryptUtil.java
+++ lune/src/main/java/com/example/lune/config/encryptUtil.java
@@ -1,5 +1,6 @@
 package com.example.lune.config;
 
+import org.apache.tomcat.util.buf.HexUtils;
 import org.springframework.util.DigestUtils;
 
 import java.security.MessageDigest;
@@ -7,7 +8,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
 public class encryptUtil {
-    public static void generator() {
+    public static String generator(String password) {
         Random random = new Random();
         int a  = random.nextInt(99999999);
         int b = random.nextInt(99999999);
@@ -22,12 +23,30 @@ public class encryptUtil {
             }
         }
         System.out.println(sb);
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+        String salt = sb.toString();
+        password = md5Hex(password + salt);
+        char[] cs = new char[48];
+        for (int i = 0; i < 48; i += 3) {
+            cs[i] = password.charAt(i / 3 * 2);
+            char c = salt.charAt(i / 3);
+            cs[i + 1] = c;
+            cs[i + 2] = password.charAt(i / 3 * 2 + 1);
         }
+        return new String(cs);
+
+
 
 
     }
+
+    private static String md5Hex(String src) {
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            byte[] bs = md5.digest(src.getBytes());
+            return new String(HexUtils.toHexString(bs));
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
 }
