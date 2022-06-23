6878086a3601968e8b2e76d3068a302c0c08e181
danta12 <name_Chloe@outlook.com>
2022-06-23 21:31:02 +0800

    client

--- lune-admin/lune-admin/src/views/client/index.vue
+++ lune-admin/lune-admin/src/views/client/index.vue
@@ -1,39 +1,47 @@
 <template>
-  <div class="video">
+  <div>
   <video playsinline autoplay muted loop poster="..\..\assets\img\videoCover.png" id="bgvid">
     <source src="..\..\assets\video\Recordplayer1_1080p_Videvo_x264.mp4" type="video/mp4">
   </video>
-  <el-header>
-    <div class= "header">
-      <div class="menu-btn" @click="handleCollapse">
-        <img height="40px" width="40px" src="../../assets/img/moon.svg"/>
-      </div>
-      <!--搜索框-->
-      <div class="header-search">
-        <input type="text" placeholder="搜索音乐" >
-      </div>
-      <div class="header-right">
-      <div class="header-user-con">
-        <!-- 用户头像 -->
-        <div class="user-avator">
-          <img :src="userPic" />
+  <el-container>
+    <el-header>
+        <div class= "header">
+            <div class="header-left">
+                <div class="menu-btn" @click="handleCollapse">
+                    <img height="40px" width="40px" src="../../assets/img/moon.svg"/>
+                </div>
+                <!--搜索框-->
+                <div class="header-search">
+                    <input type="text" placeholder="搜索音乐" >
+                </div>
+            </div>
+            <div class="header-right">
+                <div class="header-user-con">
+                    <!-- 用户头像 -->
+                    <div class="user-avator">
+                        <img :src="userPic" />
+                    </div>
+                    <!-- 用户名下拉菜单 -->
+                    <el-dropdown class="user-name" trigger="click" @command="handleCommand" >
+                        <span class="el-dropdown-link">
+                            {{username}}
+                            <i class="el-icon-caret-bottom"></i>
+                        </span>
+                        <template #dropdown>
+                            <el-dropdown-menu>
+                            <el-dropdown-item command="loginout">Log Out</el-dropdown-item>
+                            </el-dropdown-menu>
+                        </template>
+                    </el-dropdown>
+                </div>
+            </div>
         </div>
-        <!-- 用户名下拉菜单 -->
-        <el-dropdown class="user-name" trigger="click" @command="handleCommand" >
-          <span class="el-dropdown-link">
-            {{username}}
-            <i class="el-icon-caret-bottom"></i>
-          </span>
-          <template #dropdown>
-            <el-dropdown-menu>
-              <el-dropdown-item command="loginout">Log Out</el-dropdown-item>
-            </el-dropdown-menu>
-          </template>
-        </el-dropdown>
-      </div>
-    </div>
-    </div>
-  </el-header>
+
+    </el-header>
+  </el-container>
+  <div class="mainPage">
+
+  </div>
   </div>
 </template>
 
@@ -41,31 +49,13 @@
 export default {
   data () {
     return {
-      activeIndex: '/dashboard',
-      isCollapse: false,
       username: 'admin',
       userPic: require('@/assets/img/user.jpg'),
       nusicName: 'music'
     }
   },
-  mounted () {
-    this.activeIndex = this.handleSelect()
-  },
   methods: {
-    // 用户名下拉菜单选择事件
-    // 侧边栏折叠
-    handleCollapse () {
-      this.isCollapse = !this.isCollapse
-    },
-    handleSelect (key, keyPath) {
-      console.log(key, keyPath)
-      // this.activeIndex = keyPath[0]
-      console.log(this.activeIndex)
-      this.$router.push(key)
-      return key
 
-      // return keyPath[0]
-    },
     // 用户名下拉菜单选择事件
     handleCommand (command) {
       if (command === 'loginout') {
@@ -100,14 +90,16 @@ video#bgvid {
     width: 60%;
     margin-top: 10%;
 }
-el-header{
-  padding: 0;
+header-left{
+    float: left;
+    width: 500px;
 }
 .el-aside {
     line-height: 200px;
 }
 
 .header {
+  margin-left: -20px;
   position: absolute;
   z-index: 100;
   box-sizing: border-box;
@@ -117,6 +109,12 @@ el-header{
   color: #0f382a;
   box-shadow: 0px 0px 8px 2px rgba(136, 108, 108, 0.3);
   border-bottom: #9b9f9e;
+}
+.header-search {
+    margin-top: 1%;
+    margin-left: 3%;
+    float: left;
+    width: 15%;
 }
 
 .menu-btn{
@@ -175,5 +173,13 @@ menu-btn img{
   display:block;
   width:20px;
   height: 20px;
+}
+.mainPage{
+    width: 100%;
+    opacity: 90%;
+    height: 800px;
+    margin-top: 50%;
+    background-color: rgb(255, 255, 255);
+    color: white;
 }
 </style>
